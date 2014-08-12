419100e814dac03a3458094c9b771b8c337f8c1d
Siva K. Reddybathula <siva.reddybathula@gmail.com>
2014-08-12 09:55:31 -0700

    Updated

--- app/controllers/emailer_controller.rb
+++ app/controllers/emailer_controller.rb
@@ -1,6 +1,7 @@
 class EmailerController < ApplicationController
 
   def get_send_mail
+    puts "Inside get mail"
     p request.original_url
     RestClient.post "http://#{request.original_url}/send_mail", {to: "Sai Sujith <sivakr39@yahoo.com>", subject: "hello world", body: "Hi sai! Sending you an email via this API I just made."}.to_json, :content_type => :json, :accept => :json
 	RestClient.post "http://#{request.original_url}/send_mail", {to: "Sai Sujith <saisujith717@gmail.com>", subject: "hello world", body: "Hi sai! Sending you an email via this API I just made."}.to_json, :content_type => :json, :accept => :json
