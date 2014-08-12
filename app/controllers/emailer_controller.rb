404157f2ed9b2c30d938827cd59786dfdad4eda8
Siva K. Reddybathula <siva.reddybathula@gmail.com>
2014-08-12 00:21:34 -0700

    First Commit

---
+++ app/controllers/emailer_controller.rb
@@ -0,0 +1,23 @@
+class EmailerController < ApplicationController
+
+  def get_send_mail
+    RestClient.post "http://localhost:3000/send_mail", {to: "Sai Sujith <sivakr39@yahoo.com>", subject: "hello world", body: "Hi sai! Sending you an email via this API I just made."}.to_json, :content_type => :json, :accept => :json
+	RestClient.post "http://localhost:3000/send_mail", {to: "Sai Sujith <saisujith717@gmail.com>", subject: "hello world", body: "Hi sai! Sending you an email via this API I just made."}.to_json, :content_type => :json, :accept => :json
+  end
+  
+  def post_send_mail
+     toaddr, sub, mbody = params[:to], params[:subject], params[:body]	 
+
+	 Mail.deliver do
+       to        toaddr
+	   from      'noreply@example.com' 
+	   subject   sub 
+	   body      mbody 
+	 end
+	 
+	 respond_to do |format|
+      format.json { render text: 'Email Sent'}
+     end
+  end
+  
+end
